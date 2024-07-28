9db6e369b96b57ef96fcd1e008d431c13b02dd37
Chelle Clark <chelleclark308@gmail.com>
2024-07-28 12:38:12 -0700

    Add in sounds

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,8 @@ use agb::{
 use agb::sound::mixer::SoundChannel;
 
 static GRAPHICS: &Graphics = include_aseprite!("gfx/sprites.aseprite");
+static FIFTEEN_STEP: &[u8] = include_wav!("sound/15StepCrust.wav");
+static SPROING: &[u8] = include_wav!("sound/sproing.wav");
 
 static PADDLE_END: &Tag = GRAPHICS.tags().get("Paddle End");
 static PADDLE_MID: &Tag = GRAPHICS.tags().get("Paddle Mid");
@@ -56,11 +58,17 @@ impl<'obj> Paddle<'obj> {
 fn main(mut gba: agb::Gba) -> ! {
     let object = gba.display.object.get_managed();
     let mut input = agb::input::ButtonController::new();
+    let mut mixer = gba.mixer.mixer(Frequency::Hz10512);
+    mixer.enable();
     let vblank = agb::interrupt::VBlank::get();
 
     let mut ball = object.object_sprite(BALL.sprite(0));
     let mut paddle_l = Paddle::new(&object, 8, 8);
     let mut paddle_r = Paddle::new(&object, agb::display::WIDTH - 16 - 8, 8);
+
+    let mut channel = SoundChannel::new(FIFTEEN_STEP);
+    channel.stereo();
+    let _ = mixer.play_sound(channel);
 
     let mut ball_x = 50;
     let mut ball_y = 50;
@@ -77,10 +85,15 @@ fn main(mut gba: agb::Gba) -> ! {
         if input.is_pressed(Button::A) {
             x_velocity *= 2;
             y_velocity *= 2;
+            if input.is_just_pressed(Button::A) {
+                let mut channel = SoundChannel::new(SPROING);
+                let _ = mixer.play_sound(channel);
+            }
         }
 
         ball.set_x(ball_x as u16).set_y(ball_y as u16);
 
+        mixer.frame();
         vblank.wait_for_vblank();
         object.commit();
         input.update();
